dc8a2863a78300b7f2b51c3db1103f3d49a62a81
wesly <weslyambarita4@gmail.com>
2025-11-11 21:26:53 +0700

    Revert "update"
    This reverts commit 1553ca06576e8792bddedd13e5a847f82e06f546.

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TodoController; // 1. Pastikan TodoController di-import
 use Illuminate\Support\Facades\Route;
 
-// routes/web.php (KODE PERBAIKAN)
-
 Route::middleware(['handle.inertia'])->group(function () {
     // Auth Routes
     Route::group(['prefix' => 'auth'], function () {
         Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
-        // Beri nama unik, contoh: 'auth.login.store'
-        Route::post('/login', [AuthController::class, 'postLogin'])->name('auth.login.store'); 
+        Route::post('/login', [AuthController::class, 'postLogin'])->name('auth.login');
 
         Route::get('/register', [AuthController::class, 'register'])->name('auth.register');
-        // Beri nama unik, contoh: 'auth.register.store'
-        Route::post('/register', [AuthController::class, 'postRegister'])->name('auth.register.store');
+        Route::post('/register', [AuthController::class, 'postRegister'])->name('auth.register');
 
         Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
     });
@@ -27,10 +23,10 @@ Route::middleware(['handle.inertia'])->group(function () {
         // Rute untuk Create (POST)
         Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
         
-        // Rute Detail/Show
+        // 2. TAMBAHKAN BARIS INI (Rute Detail/Show)
         Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todos.show');
 
-        // Rute untuk Update (POST)
+        // Rute untuk Update (POST - kita pakai POST untuk form multipart)
         Route::post('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
         
         // Rute untuk Delete (DELETE)
@@ -40,4 +36,3 @@ Route::middleware(['handle.inertia'])->group(function () {
         Route::post('/todos/{todo}/cover', [TodoController::class, 'updateCover'])->name('todos.updateCover');
     });
 });
-// <-- Pastikan tidak ada '}' tambahan di sini
